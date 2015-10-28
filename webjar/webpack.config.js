f7050d99131cd45a2cc0daffc344d2999b1a52a6
Janos Haber <janos.haber@finesolution.hu>
2015-10-28 23:46:13 +0100

    webjar support test

---
+++ webjar/webpack.config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var webpack = require('webpack');
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+
+module.exports = {
+
+    entry: {
+        mui: './index.js',
+        tagsInput: './js/reactTagsInputPage.js',
+        selectPage: './js/reactSelectPage.js',
+        geomIcons: './js/reactGeomIconsPage.js',
+        infinite: './js/reactInfinitePage.js',
+        spinner: './js/spinnerPage.js'
+    },
+    output: {
+        path: __dirname + '/assets',
+        publicPath: "/assets/",
+        filename: '[name].js'
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new CommonsChunkPlugin({
+            name: "react-components"
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+
+            {
+                test: /\.(png|jpg|svg)$/,
+                loaders: [
+                    'url-loader?limit=8192',
+                    'image-webpack?optimizationLevel=7&progressive=true']
+            } // inline base64 URLs for <=8k images, direct URLs for the rest
+        ]
+    }
+
+
+};
