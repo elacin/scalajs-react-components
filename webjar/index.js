7aa7fc6c5e5c06071f2c7f99b267b56e63205031
Janos Haber <janos.haber@finesolution.hu>
2015-10-29 00:04:45 +0100

    Removed react and force set Added the styles

--- webjar/index.js
+++ webjar/index.js
@@ -1,10 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
 var injectTapEventPlugin = require('react-tap-event-plugin');
 
 injectTapEventPlugin();
 
-window.ReactDOM = ReactDOM;
-window.React = React;
 var mui = require("material-ui");
 window.mui = mui;
